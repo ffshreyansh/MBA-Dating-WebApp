e7f1dfe105f6ca87c66a69004aea723e92279d9c
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-01-24 10:54:07 +0530

    init3

--- app.js
+++ app.js
@@ -33,160 +33,9 @@ mongoose.connect(db, {useNewUrlParser: true})
     .then(() => console.log("MongoDB connected.."))
     .catch(err => console.log(err));
     
-
-
-//Pushing the Data into Mongo DB ====================================================================
-
-MongoClient.connect(uri, { useNewUrlParser: true }, function (err, client) {
-    if (err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
-    }
-    console.log('Connected...');
-    const db = client.db('stickman');
-
-    app.post('/', function (req, res) {
-
-        const name = req.body.name;
-        const number = req.body.number;
-        const data = number.split(',');
-
-        let num = [];
-        data.forEach(number => {
-            num.push(number + " - " + token++);
-        });
-
-        db.collection('users').findOne({ name: name }, function (err, result) {
-            if (err) {
-                console.log(err);
-            }
-            // If a document with the same name already exists, return an error
-            if (result) {
-                res.send("Data Already Exists with the Same Name!");
-            } else {
-
-                // Insert the data into the database
-                db.collection('users').insertOne({ name: name, number: num }, function (err, result) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    console.log('Data added to the collection');
-                    res.redirect('/success');
-                });
-            }
-        });
-
-    });
-        //Using Puppeteer to Generate Name Filtered PDF=====================================
-
-        app.post('/pdf/filtered', async (req, res) => {
-            // Get the filter criteria from the query parameter
-            const filter = {name: req.body.name};
-          
-            // Find the documents that match the filter criteria
-            const collection = db.collection('users');
-            const data = await collection.find(filter).toArray();
-          
-            // Generate the PDF as before
-            const html = `
-              <html>
-                <body>
-                  <h1>Data from the database</h1>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Name</th>
-                        <th>Number</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      ${data.map(row => `
-                        <tr>
-                          <td>${row.name}</td>
-                          <td>${row.number}</td>
-                        </tr>
-                      `).join('')}
-                    </tbody>
-                  </table>
-                </body>
-              </html>
-            `;
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.setContent(html);
-            const buffer = await page.pdf({ format: 'A4' });
-            await browser.close();
-          
-            // Send the PDF to the client
-            res.setHeader('Content-Type', 'stickman/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename="stickman.pdf"');    
-            res.send(buffer);
-          });
-          
-
-
-
-        // Using Puppeteer for Admin's PDF generation==================================================================
-
-        app.post('/pdf/admin', async (req, res) => {
-
-            const collection = db.collection('users');
-            // const filter = { name: 'c' };
-            // const data = await collection.find(filter).toArray();
-            const data = await collection.find().toArray();
-
-            //HTML for the PDF
-            const html = `
-  <html>
-    <body>
-      <h1>Data from the database</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${data.map(row => `
-            <tr>
-              <td>${row.name}</td>
-              <td>${row.number}</td>
-            </tr>
-          `).join('')}
-        </tbody>
-      </table>
-    </body>
-  </html>
-`;
-
-            //Generating PDF
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.setContent(html);
-            const buffer = await page.pdf({ format: 'A4' });
-            await browser.close();
-
-            //Send the PDF to the Client
-            res.setHeader('Content-Type', 'stickmanAdmin/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename="stickmanAdmin.pdf"');
-            res.send(buffer);
-
-        });
-
-
-    
-
-})
-
-
-
-
-
-
-
-    app.get('/success', (req, res) => {
-        res.sendFile(__dirname + '/success.html');
-    });
+    // app.get('/success', (req, res) => {
+    //     res.sendFile(__dirname + '/success.html');
+    // });
 
 
     // app.get('/', (req, res) => {
@@ -201,7 +50,7 @@ MongoClient.connect(uri, { useNewUrlParser: true }, function (err, client) {
 
 
 //EJS
-app.use(expressLayouts);
+// app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 //BodyParse
@@ -240,10 +89,3 @@ app.use('/users', require("./routes/users.js"));
 
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
-
-process.on('SIGINT', () => {
-    client.close();
-    console.log('Disconnected from MongoDB');
-    process.exit(0);
-});
